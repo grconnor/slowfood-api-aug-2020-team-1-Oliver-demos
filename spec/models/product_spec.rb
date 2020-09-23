523f66c550c24f37658db2492b10d7942f8ce29e
FacundoOs <osores.facundo@gmail.com>
2020-09-23 17:26:31 +0200

    add unit test for product model.Add validate_presence_of name and price in product model. Problem with rubocop extension. Can't format code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid Factory" do
+    expect(create(:product)).to be_valid
+end
+
+describe "Database table" do
+  it { is_expected.to have_db_column :name }
+  it { is_expected.to have_db_column :price }
+  it { is_expected.to have_db_column :description }
+end
+
+describe "Validations" do
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :price }
+end
 end
